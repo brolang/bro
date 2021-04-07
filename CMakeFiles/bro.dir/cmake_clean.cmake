2df04421379f3d7468fecc9203034231dbbc5eb3
Krishna Satya <vadapallikrishna@gmail.com>
2021-04-07 09:13:12 +0530

    cmake

---
+++ CMakeFiles/bro.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bro.dir/src/compiler/bro.c.o"
+  "bro"
+  "bro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/compiler/bro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("BRO LANGUAGE\n");
+    printf("VADAPALLI KRISHNA SATYA\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("compile compile the file\n");
+    printf("eval run the interpreter\n");
+    printf("download the dependencies\n");
+    printf("run the file\n");
+}
