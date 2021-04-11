dc971b6a54557a5880a26f533054a13084e21092
Krishna Satya <vadapallikrishna@gmail.com>
2021-04-11 11:55:49 +0530

    remove ctest

--- CTestTestfile.cmake
+++ CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /data/data/com.termux/files/home/bro
-# Build directory: /data/data/com.termux/files/home/bro
+# Source directory: /data/data/com.termux/files/home/ping
+# Build directory: /data/data/com.termux/files/home/ping
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
